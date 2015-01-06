007cc07d56b781f70277183f5b4e8f52a5cf7bf7
jwtucker <tuckerjeremyw@gmail.com>
2015-01-06 17:28:04 -0500

    Product page CSS

--- app/models/item.js
+++ app/models/item.js
@@ -5,6 +5,8 @@ var mongoose = require('mongoose');
 // define our item model
 // module.exports allows us to pass this to other files when it is called
 module.exports = mongoose.model('Item', {
-    name : {type : String, default: ''},
-    brand : {type: Array, default: []}
+    name : 			{type : String, default: 'Item'},
+    brand : 		{type: Array, default: ['No Brand']},
+    price : 		{type: Number, default: 0.00},
+    description : 	{type: String, default: 'No Description'}
 });
